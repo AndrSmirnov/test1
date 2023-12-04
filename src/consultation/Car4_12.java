ee4bdf5409f51fa739dce996217403c4e3a89b2b
AndrSmirnov <asmirnoff.1974@gmail.com>
2023-12-04 18:24:24 +0100

    HomeWork29_11

---
+++ src/consultation/Car4_12.java
@@ -0,0 +1,21 @@
+package consultation;
+
+public class Car4_12 {
+    public static void main(String[] args) {
+        Car4_12  ford = new Car4_12();
+        ford.condition = true;
+    }
+   public String brand;
+
+     String model;
+
+    private boolean condition;      // true - new,  false - old
+
+    public int productionYear;
+
+    private double km;
+
+}
+
+
+
---
+++ src/consultation/Main4_12.java
@@ -0,0 +1,11 @@
+package consultation;
+
+public class Main4_12 {
+    public static void main(String[] args) {
+        Car4_12  volvo = new Car4_12();
+
+         volvo.brand = "Volvo";
+         volvo.model = "S80";
+         volvo.productionYear = 2010;
+    }
+}
