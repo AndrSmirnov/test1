6ff2bb561967aeeb6828a1c035144821bf966e3d
AndrSmirnov <asmirnoff.1974@gmail.com>
2024-01-28 18:52:09 +0100

    HW 41_20_01

---
+++ src/hwLesson41/Homework.java
@@ -0,0 +1,28 @@
+package hwLesson41;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class Homework {
+    /*
+   Задача на мапы: Дан список имен, в котором некоторые имена повторяются.
+   Написать метод, который по имени вернет количество вхождений этого имени
+   в список. т.е. метод принимает лист и имя и возвращает число,
+   соответствующее количеству раз , которое это имя встречается в листе
+
+   John, Mary, Ann, Mary,John,John
+    */
+    // John=3, Mary=2, Ann=1,
+    public int findNameToNumberOccurence(List<String> strings, String name){
+        Map<String,Integer> nameToNumber = new HashMap<>();
+
+        for(String s : strings){
+            if( nameToNumber.containsKey(s))
+                nameToNumber.put(s, nameToNumber.get(s) +1 );
+            else nameToNumber.put(s,1);
+        }
+
+        return nameToNumber.getOrDefault(name, 0);
+    }
+}
